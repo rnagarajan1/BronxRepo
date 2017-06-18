1007304a239c18ba3693a365ba666728d62c1368
rnagarajan1 <macsrangs@gmail.com>
2017-06-18 01:17:55 -0400

    update variables toi bx instead of bk

--- Bornx_Tidy_Data-Ranga.R
+++ Bornx_Tidy_Data-Ranga.R
@@ -1,4 +1,4 @@
-# Author: Benjamin Reddy
+# Author: Rangaswamy Nagarajan
 # Taken from pages 49-50 of O'Neil and Schutt
 
 
@@ -8,12 +8,12 @@ setwd("C:\\Users\\swara\\OneDrive\\Documents\\MSDS\\Data Science\\Unit 6\\")
 
 
 # read csv file
-bk <- read.csv("rollingsales_bronx.csv",skip=4,header=TRUE)
+bx <- read.csv("rollingsales_bronx.csv",skip=4,header=TRUE)
 
 ## Check the data
-head(bk)
-summary(bk)
-str(bk) # Very handy function!
+head(bx)
+summary(bx)
+str(bx) # Very handy function!
 #Compactly display the internal structure of an R object.
 
 
@@ -23,36 +23,36 @@ str(bk) # Very handy function!
 ## start with digits. We use the gsub command to replace them with a blank space.
 # We create a new variable that is a "clean' version of sale.price.
 # And sale.price.n is numeric, not a factor.
-bk$SALE.PRICE.N <- as.numeric(gsub("[^[:digit:]]","", bk$SALE.PRICE))
+bx$SALE.PRICE.N <- as.numeric(gsub("[^[:digit:]]","", bx$SALE.PRICE))
 #count(is.na(bk$SALE.PRICE.N))
 
-names(bk) <- tolower(names(bk)) # make all variable names lower case
+names(bx) <- tolower(names(bx)) # make all variable names lower case
 
 ## Get rid of leading digits
-bk$gross.sqft <- as.numeric(gsub("[^[:digit:]]","", bk$gross.square.feet))
-bk$land.sqft <- as.numeric(gsub("[^[:digit:]]","", bk$land.square.feet))
-bk$year.built <- as.numeric(as.character(bk$year.built))
+bx$gross.sqft <- as.numeric(gsub("[^[:digit:]]","", bx$gross.square.feet))
+bx$land.sqft <- as.numeric(gsub("[^[:digit:]]","", bx$land.square.feet))
+bx$year.built <- as.numeric(as.character(bx$year.built))
 
 ## do a bit of exploration to make sure there's not anything
 ## weird going on with sale prices
-attach(bk)
+attach(bx)
 hist(sale.price.n) 
-detach(bk)
+detach(bx)
 
 ## keep only the actual sales
 
-bk.sale <- bk[bk$sale.price.n!=0,]
-plot(bk.sale$gross.sqft,bk.sale$sale.price.n)
-plot(log10(bk.sale$gross.sqft),log10(bk.sale$sale.price.n))
+bx.sale <- bx[bx$sale.price.n!=0,]
+plot(bx.sale$gross.sqft,bx.sale$sale.price.n)
+plot(log10(bx.sale$gross.sqft),log10(bx.sale$sale.price.n))
 
 ## for now, let's look at 1-, 2-, and 3-family homes
-bk.homes <- bk.sale[which(grepl("FAMILY",bk.sale$building.class.category)),]
-dim(bk.homes)
-plot(log10(bk.homes$gross.sqft),log10(bk.homes$sale.price.n))
-summary(bk.homes[which(bk.homes$sale.price.n<100000),])
+bx.homes <- bx.sale[which(grepl("FAMILY",bx.sale$building.class.category)),]
+dim(bx.homes)
+plot(log10(bx.homes$gross.sqft),log10(bx.homes$sale.price.n))
+summary(bx.homes[which(bx.homes$sale.price.n<100000),])
 ""
 
 ## remove outliers that seem like they weren't actual sales
-bk.homes$outliers <- (log10(bk.homes$sale.price.n) <=5) + 0
-bk.homes <- bk.homes[which(bk.homes$outliers==0),]
-plot(log10(bk.homes$gross.sqft),log10(bk.homes$sale.price.n))
+bx.homes$outliers <- (log10(bx.homes$sale.price.n) <=5) + 0
+bx.homes <- bx.homes[which(bx.homes$outliers==0),]
+plot(log10(bx.homes$gross.sqft),log10(bx.homes$sale.price.n))
